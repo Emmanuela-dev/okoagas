f162c0b6d422a29f26e67f7ad391f3b06f612f5b
Emmanuela-dev <omondiemmanuela7@gmail.com>
2026-05-14 17:30:55 +0300

    changes

--- src/pages/GetKit.jsx
+++ src/pages/GetKit.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { User, MapPin, Package, CreditCard, ChevronRight, ChevronLeft, Check, Loader2 } from 'lucide-react';
 
